2df16b1ed58faff9428d18818cd7ebdf833cacfc
jomdab <shupak999@gmail.com>
2024-02-20 15:50:08 +0700

    save la

--- cluster.py
+++ cluster.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 import cv2
+import joblib
+from matplotlib.colors import ListedColormap
+from sklearn.neighbors import KNeighborsClassifier
+
+
 
 
 def plot_(x,y1,y2,row,col,ind,title,xlabel,ylabel,label,isimage=False,color='b'):
@@ -80,4 +85,73 @@ for k in K:
                 iter+=1
             if iter>=5: break
         plt.show()
-    print() 
+    print()
+    
+#Training the model with optimial K value (6 in our case)
+kmeans = KMeans(n_clusters = 6, random_state=0).fit(X_encoded_reshape)
+labels=kmeans.labels_
+centroids = kmeans.cluster_centers_
+
+kmeans_file = '/content/drive/My Drive/kmeans_model.pkl'
+joblib.dump(kmeans,kmeans_file)
+
+clusters_features = []
+cluster_files=[]
+for i in [0,1,2,3,4,5]:
+    i_cluster = []
+    i_labels=[]
+    for iter,j in enumerate(kmeans.labels_):
+        if j==i:
+            i_cluster.append(X_encoded_reshape[iter])
+            i_labels.append(lisp[iter])
+    i_cluster = np.array(i_cluster)
+    clusters_features.append(i_cluster)
+    cluster_files.append(i_labels)
+
+labels=[]
+data=[]
+files=[]
+for iter,i in enumerate(clusters_features):
+    data.extend(i)
+    labels.extend([iter for i in range(i.shape[0])])
+    files.extend(cluster_files[iter])
+print(np.array(labels).shape)
+print(np.array(data).shape)
+print(np.array(files).shape)
+
+# Finding Nearest Neighbors(K-NN)
+
+for i in [[3,5,7],[9,11,13]]:
+    plt.figure(figsize=(25,5))
+    for iter,j in enumerate(i):
+        n_neighbors = j
+        X = values  
+        y = labels
+        h = .09  # step size in the mesh
+        cmap_light = ListedColormap(['#FFB6C1', '#AAFFAA', '#AAAAFF','#E6E6FA','#8FBC8F','#DCDCDC'])
+        cmap_bold = ListedColormap(['#F08080', '#00FF00', '#0000FF','#ADD8E6','#2F4F4F','#808080'])
+        clf = KNeighborsClassifier(n_neighbors)
+        clf.fit(X, y)
+
+        x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1 
+        y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1 
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+        plt.subplot(1,3,iter+1)
+        Z = Z.reshape(xx.shape)
+        plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+        # Plot also the training points 
+        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.title("For K = {} as neighbors".format(j))
+    plt.show()    
+
+knn = KNeighborsClassifier(n_neighbors=9,algorithm='ball_tree',n_jobs=-1)
+knn.fit(np.array(data),np.array(labels))
+
+knn_file = '/content/drive/My Drive/knn_model.pkl'
+joblib.dump(knn,knn_file)
+
